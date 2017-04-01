e482763ac32c46a45ce8aff167f4d632aef26dad
Sidhant <sidhant.sa@gmail.com>
2017-04-01 16:45:57 -0700

    modified appJS.js fo try

--- js/appJS.js
+++ js/appJS.js
@@ -1,15 +1,12 @@
 // closure
 (function(){
-  //model
-  var app = angular.module("gemStore", []);
-  //controller
-  app.controller("GemController", function(){
-    this.product = gem;
-  });
-  //variable
   var gem = {
     name: "Sid's Gem",
     value: "49.99",
     description: "This is my gem."
   };
+  var app = angular.module('gemStore', []);
+  app.controller("StoreController", function($scope){
+    $scope.product = gem;
+  });
 })();
