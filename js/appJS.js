fa45f83fa3246b8ea6d6218f58a6ecf667025ed0
Sidhant <sidhant.sa@gmail.com>
2017-04-01 20:06:07 -0700

    added images from web

--- js/appJS.js
+++ js/appJS.js
@@ -5,13 +5,15 @@
       name: "Sid's Gem",
       value: "49.99",
       description: "This is my gem.",
-      canPurchase: true
+      canPurchase: true,
+      image: "images/boy.jpg"
     },
     {
       name: "Sun's Gem",
       value: "99.99",
       description: "This is her gem.",
-      canPurchase: true
+      canPurchase: true,
+      image: "images/girl.jpg"
     }
 ];
   var app = angular.module('gemStore', []);
