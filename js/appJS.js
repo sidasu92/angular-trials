d9f1f28ef239f9b5c3f9567f36a2fa4316f8b733
Sidhant <sidhant.sa@gmail.com>
2017-04-02 00:05:13 -0700

    modified page and data

--- js/appJS.js
+++ js/appJS.js
@@ -6,14 +6,21 @@
       value: "49.99",
       description: "This is my gem.",
       canPurchase: true,
-      image: "images/boy.jpg"
+      image: "images/boy.jpg",
+      description: "I am Sidhant's avatar. Lorem ipsum pifsum",
+      specification: "",
+      review: "One of it's kind."
+
     },
     {
       name: "Sun's Gem",
       value: "99.99",
       description: "This is her gem.",
       canPurchase: true,
-      image: "images/girl.jpg"
+      image: "images/girl.jpg",
+      description: "This is Sun's avatar. Lorem ipsum pifsum",
+      specification: "",
+      review: "Unmatched!"
     }
 ];
   var app = angular.module('gemStore', []);
