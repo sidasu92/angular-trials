3d36f8f9036d546561c887ce76891c7d62303c43
Sidhant <sidhant.sa@gmail.com>
2017-04-06 20:15:35 -0700

    added dynamic review

--- js/appJS.js
+++ js/appJS.js
@@ -1,73 +1,77 @@
 // closure
-(function(){
-  var gems = [
-    {
-      name: "Sid's Gem",
-      value: "49.99",
-      description: "This is my gem.",
-      canPurchase: true,
-      image: "images/boy.jpg",
-      description: "I am Sidhant's avatar. Lorem ipsum pifsum",
-      specification: "",
-      reviews: [
-        {
-          stars: 5,
-          body: "I love <3",
-          author: "sid@asu.com"
+(function() {
+    var gems = [{
+            name: "Sid's Gem",
+            value: "49.99",
+            description: "This is my gem.",
+            canPurchase: true,
+            image: "images/boy.jpg",
+            description: "I am Sidhant's avatar. Lorem ipsum pifsum",
+            specification: "",
+            reviews: [{
+                    stars: 5,
+                    body: "I love <3",
+                    author: "sid@asu.com"
+                },
+                {
+                    stars: 5,
+                    body: "Aweosmely awesome!!!",
+                    author: "asu@sid.com"
+                }
+            ]
+
         },
         {
-          stars: 5,
-          body: "Aweosmely awesome!!!",
-          author: "asu@sid.com"
+            name: "Sun's Gem",
+            value: "99.99",
+            description: "This is her gem.",
+            canPurchase: true,
+            image: "images/girl.jpg",
+            description: "This is Sun's avatar. Lorem ipsum pifsum",
+            specification: "",
+            reviews: [{
+                    stars: 5,
+                    body: "Wow, just WOW!",
+                    author: "sid@asu.com"
+                },
+                {
+                    stars: 5,
+                    body: "<3 love <3",
+                    author: "asu@sid.com"
+                }
+            ]
         }
-      ]
+    ];
+    var app = angular.module('gemStore', []);
+    app.controller("StoreController", function() {
+        this.products = gems;
+    });
 
-    },
-    {
-      name: "Sun's Gem",
-      value: "99.99",
-      description: "This is her gem.",
-      canPurchase: true,
-      image: "images/girl.jpg",
-      description: "This is Sun's avatar. Lorem ipsum pifsum",
-      specification: "",
-      reviews: [
-        {
-          stars: 5,
-          body: "Wow, just WOW!",
-          author: "sid@asu.com"
-        },
-        {
-          stars: 5,
-          body: "<3 love <3",
-          author: "asu@sid.com"
-        }
-      ]
-    }
-];
-  var app = angular.module('gemStore', []);
-  app.controller("StoreController", function(){
-    this.products = gems;
-  });
+    app.controller("PanelController", function() {
+        // initialize tab = 1
+        //property of the tab
+        this.tab = 1;
 
-  app.controller("PanelController", function(){
-    // initialize tab = 1
-    //property of the tab
-    this.tab = 1;
+        //function expression to set tab values on click
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
 
-    //function expression to set tab values on click
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
+        //function expression to set the tab as active
+        this.isSelectedTab = function(curTab) {
+            return curTab === this.tab;
+        };
+    });
 
-    //function expression to set the tab as active
-    this.isSelectedTab = function(curTab) {
-      return curTab === this.tab;
-    };
-  });
+    app.controller("ReviewController", function() {
+        // empty object
+        this.review = {};
 
-  app.controller("ReviewController", function(){
-// empty object
-    this.review = {};
-  });
+        // to add review in the products
+        this.addReview = function(product) {
+          product.reviews.push(this.review);
+          //clear the review object
+          this.review = {};
+        };
+    });
 })();
